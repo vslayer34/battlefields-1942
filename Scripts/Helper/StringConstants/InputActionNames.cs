19806fb7d335c1f3e324c647d10d2c5abae4fe88
Mohamed El-Nagar <mohamd.elnagar.34@gmail.com>
2026-04-27 15:04:59 +0300

    Added the input actions in the project settings and the helper class for referenceing them

---
+++ Scripts/Helper/StringConstants/InputActionNames.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+namespace BattleField1942.Scripts.Helper.Constants;
+public static class InputActionNames
+{
+    public static readonly StringName MOVE_FORWARD = "move_forward";
+    public static readonly StringName MOVE_BACKWARD = "move_backward";
+    public static readonly StringName MOVE_RIGHT = "move_right";
+    public static readonly StringName MOVE_LEFT = "move_left";
+    public static readonly StringName JUMP = "jump";
+    public static readonly StringName CROUCH_TOGGLE = "crouch_toggle";
+    public static readonly StringName PRONE_TOGGLE = "prone_toggle";
+}
